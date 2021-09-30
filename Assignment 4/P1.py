6b429a50efe6f12d10567c0b3eedea323f9d70c8
lucifer-T56 <91344724+lucifer-T56@users.noreply.github.com>
2021-09-30 20:15:28 +0530

    Add files via upload

---
+++ Assignment 4/P1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 14:17:50 2021
+
+@author: pc
+"""
+
+stack_in_python = []
+ 
+
+stack_in_python.append("w")
+stack_in_python.append("io")
+stack_in_python.append(98)
+stack_in_python.append("d")
+stack_in_python.append(3)
+stack_in_python.append("q")
+print("After push all elements in list\nlist ::")
+print(stack_in_python)
+print("pop elemnt one by one ")
+print(stack_in_python.pop())
+print(stack_in_python.pop())
+print(stack_in_python.pop())
+print(stack_in_python.pop())
+print(stack_in_python.pop())
