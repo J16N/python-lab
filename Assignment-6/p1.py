0e384a477b06b6eb25678ee5e32f0ed5c8881de3
Tsubasa <J16N@users.noreply.github.com>
2021-11-11 10:00:47 +0000

    Modified problem-1

--- Assignment-6/p1.py
+++ Assignment-6/p1.py
@@ -4,7 +4,11 @@ def starValue(arr, n):
         star = star + 1 if (i % n == 0) else star
     return star
 
-getStarValues = lambda arr: [starValue(arr[0:i], v) for i, v in enumerate(arr)]
+def getStarValues(arr): 
+    star_values = [starValue(arr[0:i], v) for i, v in enumerate(arr)]
+    for a, b in zip(arr, star_values):
+        print(f"Star value of {a} = {b}")
+    return star_values
 
 num_array = list(map(int, input("Enter the space-seperated number sequence: ").split()))
 print(f"Maximum Star Value: { max( getStarValues(num_array) ) }")
